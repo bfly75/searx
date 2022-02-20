9103aaaf762fd626a5d6f2c65dde71cc66fe9dec
bfly75 <1951157+bfly75@users.noreply.github.com>
2022-02-20 14:23:56 +0700

    Update seafile.py
    Clean up of irrelevant query types

--- searx/engines/seafile.py
+++ searx/engines/seafile.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 """
- Elasticsearch
+ Seafile Pro using ElasticSearch
 """
 
 from json import loads, dumps
@@ -12,50 +12,27 @@ username = ''
 password = ''
 index = ''
 search_url = base_url + '/' + index + '/_search'
-query_type = 'match'
-custom_query_json = {}
 show_metadata = False
 categories = ['general']
 
 
 def init(engine_settings):
-    if 'query_type' in engine_settings and engine_settings['query_type'] not in _available_query_types:
-        raise ValueError('unsupported query type', engine_settings['query_type'])
-
     if index == '':
         raise ValueError('index cannot be empty')
 
 
 def request(query, params):
-    if query_type not in _available_query_types:
-        return params
-
     if username and password:
         params['auth'] = (username, password)
 
     params['url'] = search_url
     params['method'] = 'GET'
-    params['data'] = dumps(_available_query_types[query_type](query))
+    params['data'] = dumps(_simple_query_string_query(query))
     params['headers']['Content-Type'] = 'application/json'
 
     return params
 
-
-def _match_query(query):
-    """
-    The standard for full text queries.
-    searx format: "key:value" e.g. city:berlin
-    REF: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-match-query.html
-    """
-
-    try:
-        key, value = query.split(':')
-    except Exception as e:
-        raise ValueError('query format must be "key:value"') from e
-
-    return {"query": {"match": {key: {'query': value}}}}
-
-
+   
 def _simple_query_string_query(query):
     """
     Accepts query strings, but it is less strict than query_string
@@ -63,48 +40,8 @@ def _simple_query_string_query(query):
     REF: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-simple-query-string-query.html
     """
 
+    #TODO RC: evaluate query options
     return {'query': {'simple_query_string': {'query': query}}}
-
-
-def _term_query(query):
-    """
-    Accepts one term and the name of the field.
-    searx format: "key:value" e.g. city:berlin
-    REF: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-term-query.html
-    """
-
-    try:
-        key, value = query.split(':')
-    except Exception as e:
-        raise ValueError('query format must be key:value') from e
-
-    return {'query': {'term': {key: value}}}
-
-
-def _terms_query(query):
-    """
-    Accepts multiple terms and the name of the field.
-    searx format: "key:value1,value2" e.g. city:berlin,paris
-    REF: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-terms-query.html
-    """
-
-    try:
-        key, values = query.split(':')
-    except Exception as e:
-        raise ValueError('query format must be key:value1,value2') from e
-
-    return {'query': {'terms': {key: values.split(',')}}}
-
-
-def _custom_query(query):
-    key, value = query.split(':')
-    custom_query = custom_query_json
-    for query_key, query_value in custom_query.items():
-        if query_key == '{{KEY}}':
-            custom_query[key] = custom_query.pop(query_key)
-        if query_value == '{{VALUE}}':
-            custom_query[query_key] = value
-    return custom_query
 
 
 def response(resp):
@@ -114,6 +51,7 @@ def response(resp):
     if 'error' in resp_json:
         raise SearxEngineAPIException(resp_json['error'])
 
+    #TODO RC: improve results, key-value is not the prefered way of presenting.  
     for result in resp_json['hits']['hits']:
         r = {key: str(value) if not key.startswith('_') else value for key, value in result['_source'].items()}
         r['template'] = 'key-value.html'
@@ -126,19 +64,3 @@ def response(resp):
         results.append(r)
 
     return results
-
-
-_available_query_types = {
-    # Full text queries
-    # https://www.elastic.co/guide/en/elasticsearch/reference/current/full-text-queries.html
-    'match': _match_query,
-    'simple_query_string': _simple_query_string_query,
-
-    # Term-level queries
-    # https://www.elastic.co/guide/en/elasticsearch/reference/current/term-level-queries.html
-    'term': _term_query,
-    'terms': _terms_query,
-
-    # Query JSON defined by the instance administrator.
-    'custom': _custom_query,
-}
