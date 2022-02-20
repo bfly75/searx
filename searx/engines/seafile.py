c71be3ca769f95a0fea5bb14126bf7bdf3459125
bfly75 <1951157+bfly75@users.noreply.github.com>
2022-02-20 21:18:11 +0700

    Update seafile.py
    Punctuation and formatting errors

--- searx/engines/seafile.py
+++ searx/engines/seafile.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 base_url = 'http://localhost:9200'
 index = 'repofiles'
-search_url = f"{base_url}/{index}/_search}"
+search_url = f"{base_url}/{index}/_search"
 result_url = 'https://localhost'
 categories = ['general']
 
@@ -27,7 +27,7 @@ def request(query, params):
 
     return params
 
-   
+
 def _simple_query_string_query(query):
     """
     Accepts query strings, but it is less strict than query_string
@@ -35,16 +35,17 @@ def _simple_query_string_query(query):
     REF: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-simple-query-string-query.html
     """
 
-    #TODO RC: evaluate query options
+    # TODO RC: evaluate query options
     return {
         'query': {
             'simple_query_string': {
-                'query': query, 
-                'fields': ['content', 'filename'], 
+                'query': query,
+                'fields': ['content', 'filename'],
                 'default_operator': 'and'
             }
         }
     }
+
 
 def response(resp):
     results = []
@@ -53,9 +54,9 @@ def response(resp):
     if 'error' in resp_json:
         raise SearxEngineAPIException(resp_json['error'])
 
-    #TODO RC: improve results, key-value is not the prefered way of presenting.      
+    # TODO RC: improve results, key-value is not the prefered way of presenting.      
     for result in resp_json['hits']['hits']:
-        #TODO RC: what to do with folders?
+        # TODO RC: what to do with folders?
         if result['_type'] == "file":
             url = f"{result_url}/lib/{result['_id']}"
             title = result['_source']['filename']
@@ -64,12 +65,20 @@ def response(resp):
             #   {% if result.publishedDate %}<time class="published_date" datetime="{{ result.pubdate }}" >{{ result.publishedDate }}</time>{% endif %}
             # Not clear though what result.pubdate is for. The other engines use publishedDate.
             publishedDate = datetime.fromtimestamp(result['_source']['mtime'])
-            metadata = {'score': result['_score']
-                        'size': result['_source']['size']
+            metadata = {
+                'score': result['_score'],
+                'size': result['_source']['size']
             }
-            #TODO RC: consider to make a new template, because metadata are not shown otherwise? Although not that import. Look at merging Default and Torrent.
+            # TODO RC: consider to make a new template, because metadata are not shown otherwise? Although not that import. Look at merging Default and Torrent.
             template = 'default.html'
-            
-            results.append({'template': template, 'url': url, 'title': title, 'content': content, 'publishedDate': publishedDate 'metadata': metadata})        
+
+            results.append( {
+                'template': template, 
+                'url': url, 
+                'title': title, 
+                'content': content, 
+                'publishedDate': publishedDate,
+                'metadata': metadata
+            } )
 
     return results
